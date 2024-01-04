11c00c78d3447944734e6ea78da8d04d50f838bd
Pavel <pavelvodocnikov@gmail.com>
2024-01-04 16:50:23 +0500

    update app

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
-from events import Event
+from .events import Event
+from .vk import Vk
+from .database import Settings
 
 
 app = Flask(__name__)
+vk = Vk()
 
 
 @app.route("/", methods=['POST'])
