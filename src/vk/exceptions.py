cf419f85a528c3bc94e3acfd70af460aba845db2
Pavel <pavelvodocnikov@gmail.com>
2024-01-04 17:31:12 +0500

    update app, vk

--- src/vk/exceptions.py
+++ src/vk/exceptions.py
@@ -1,2 +1,15 @@
-class VkError:
+class VkError(Exception):
     ...
+
+
+class VkResponseError(Exception):
+    def __init__(self, error_code, error_msg) -> None:
+        self.error_code = error_code
+        self.error_msg = error_msg
+
+    def __str__(self) -> str:
+        return f"({self.error_code}) {self.error_msg}"
+
+
+class ResponseError(Exception):
+    pass
--- src/vk/vk.py
+++ src/vk/vk.py
@@ -1,6 +1,6 @@
 import requests
 
-from .exceptions import VkError
+from .exceptions import VkError, VkResponseError, ResponseError
 
 
 class Vk:
@@ -8,6 +8,26 @@ class Vk:
         self.token = token
         self.version = version
         self.api_url = "https://api.vk.com/method/"
+
+    def request(self, url: str, data: dict, method: bool = False):
+        if method:
+            response = requests.post(url, data)
+        else:
+            response = requests.get(url, data)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            if data.get("response"):
+                return data['response']
+            elif data.get("error"):
+                raise VkResponseError(
+                    data['error']['error_code'], data['error']['error_msg']
+                )
+            else:
+                return data
+        else:
+            raise ResponseError("Status code is not 200")
 
     def method(self, method: str, params: dict):
         ...
